dac1893c5b29c290a1a12815e23a13062b42c184
Ihar <Igor.shulgan2013@gmail.com>
2019-01-08 13:34:59 +0100

    Load a code before any change was done

--- src/handlers/EclipseLoggerStartup.java
+++ src/handlers/EclipseLoggerStartup.java
@@ -24,33 +24,33 @@ import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.eclipse.ui.texteditor.ITextEditor;
 
 public class EclipseLoggerStartup extends AbstractUIPlugin implements IStartup {
-	
+
 	private Metric metric;
 	private HashMap<String, CodeMetric> codeMetrics = new HashMap<String, CodeMetric>();
 	private HashMap<IWorkbenchPart, IPropertyListener> partListeners = new HashMap<IWorkbenchPart, IPropertyListener>();
 	private HashMap<IWorkbenchPage, IPartListener2> pageListeners = new HashMap<IWorkbenchPage, IPartListener2>();
-	
+
 	@Override
 	public void earlyStartup() {
 		sendOfflineData();
 		IWorkbench wb = PlatformUI.getWorkbench();
 		wb.addWindowListener(generateWindowListener());
 		checkUnsendData();
-		
-		ICommandService service = (ICommandService) wb.getService(ICommandService.class);
-		
-	    IExecutionListener executionListener = new IExecutionListener() {
+
+		ICommandService service = wb.getService(ICommandService.class);
+
+		IExecutionListener executionListener = new IExecutionListener() {
 
 			@Override
 			public void notHandled(String commandId, NotHandledException exception) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
 			public void postExecuteFailure(String commandId, ExecutionException exception) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
@@ -58,35 +58,35 @@ public class EclipseLoggerStartup extends AbstractUIPlugin implements IStartup {
 				Metric metric = new Metric(commandId, "eclipse_executed_command");
 				metric.finish();
 				saveMetric(metric);
-				
+
 			}
 
 			@Override
 			public void preExecute(String commandId, ExecutionEvent event) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 		};
-	    service.addExecutionListener(executionListener);
+		service.addExecutionListener(executionListener);
 	}
-	
+
 	private void checkUnsendData() {
 		final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-	    executorService.scheduleAtFixedRate(
-	    		new Runnable() {
-	    	        @Override
-	    	        public void run() {
-	    				sendOfflineData();
-	    	        }},
-	    		0, 100, TimeUnit.SECONDS);
+		executorService.scheduleAtFixedRate(
+				new Runnable() {
+					@Override
+					public void run() {
+						sendOfflineData();
+					}},
+				0, 100, TimeUnit.SECONDS);
 	}
-	
+
 	private void saveMetric(Metric metric) {
 		Database db = Database.getDB();
 		db.insertNewMetric(metric);
 	}
-	
+
 	private void sendOfflineData() {
 		Database db = Database.getDB();
 		List<Metric> metrics = db.getMetrics();
@@ -96,9 +96,9 @@ public class EclipseLoggerStartup extends AbstractUIPlugin implements IStartup {
 		if (Server.getInstance().sendMetrics(metrics)) {
 			db.deleteMetrics(metrics);
 		}
-		
+
 	}
-	
+
 	private void storeNewMetric(String name) {
 		if (this.metric == null) {
 			this.metric = new Metric(name);
@@ -108,16 +108,16 @@ public class EclipseLoggerStartup extends AbstractUIPlugin implements IStartup {
 			this.metric = new Metric(name);
 		}
 	}
-	
+
 	private void stopMetric() {
 		if (this.metric != null) {
 			this.metric.finish();
 			saveMetric(metric);
 			this.metric = null;
 		}
-		
+
 	}
-	
+
 	private void storeNewCodeMetric(String fileName, String code) {
 		CodeMetric codeMetric = codeMetrics.get(fileName);
 		if (codeMetric == null) {
@@ -157,7 +157,7 @@ public class EclipseLoggerStartup extends AbstractUIPlugin implements IStartup {
 				addCodeListener(window.getActivePage().getActivePart());
 				addPageListener(window.getActivePage());
 			}
-			
+
 			private void addPageListener(IWorkbenchPage page) {
 				IPartListener2 listener = pageListeners.get(page);
 				if (listener == null) {
@@ -165,12 +165,26 @@ public class EclipseLoggerStartup extends AbstractUIPlugin implements IStartup {
 					page.addPartListener(listener);
 					pageListeners.put(page, listener);
 				}
+
+			}
 				
+			private String getTextFromFile(ITextEditor source) {
+				ITextEditor textEditor = (ITextEditor)source;
+
+				IDocumentProvider provider = textEditor.getDocumentProvider();
+
+				IEditorInput input = textEditor.getEditorInput();
+
+				IDocument document = provider.getDocument(input);
+
+				String text = document.get();
+
+				return text;
 			}
 			
 			private void addCodeListener(IWorkbenchPart part) {
 				String title = part.getTitle();
-
+				
 				IPropertyListener listener = partListeners.get(part);
 				if (listener == null) {
 					listener = new IPropertyListener() {
@@ -178,26 +192,22 @@ public class EclipseLoggerStartup extends AbstractUIPlugin implements IStartup {
 						@Override
 						public void propertyChanged(Object source, int propId) {
 							if (source instanceof ITextEditor)
-							 {
-							   ITextEditor textEditor = (ITextEditor)source;
-
-							   IDocumentProvider provider = textEditor.getDocumentProvider();
-
-							   IEditorInput input = textEditor.getEditorInput();
-
-							   IDocument document = provider.getDocument(input);
-
-							   String text = document.get();
-
-							   storeNewCodeMetric(title, text);
-							 }
+							{
+								String text = getTextFromFile((ITextEditor) source);
+								storeNewCodeMetric(title, text);
+							}
 						}
 					};
+
+					if (part instanceof ITextEditor) {
+						String text = getTextFromFile((ITextEditor) part);
+						storeNewCodeMetric(title, text);
+					}
 					part.addPropertyListener(listener);
 					partListeners.put(part, listener);
 				}
 			}
-			
+
 			private IPartListener2 generateIPartListener2() {
 				return new IPartListener2() {
 
